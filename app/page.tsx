d5a460883867f1f7f0a7bd67f7a6187c62b2aa77
Momen Yasser <momenyasser522@gmail.com>
2025-10-31 17:21:43 +0200

    Update layout and home page: Changed metadata in layout.tsx to reflect personal branding and adjusted layout structure in page.tsx for improved spacing and organization of components.

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,11 @@ import Skills from '@/components/skills'
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-gray-950 text-white font-sans">
-      <Hero />
-      <Skills />
+      <div className="flex flex-col py-40">
+        <Hero />
+        <Skills />
+      </div>
+
       <Projects />
       <Contact />
     </div>
